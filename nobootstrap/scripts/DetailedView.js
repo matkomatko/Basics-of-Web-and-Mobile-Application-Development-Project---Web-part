654e1c97955aab7d478bcf3051c125e88e5bb65b
matkomatko <matko.matko@hotmail.com>
2020-02-02 23:14:45 +0100

    nobootstrap done

--- nobootstrap/scripts/DetailedView.js
+++ nobootstrap/scripts/DetailedView.js
@@ -1,15 +1,14 @@
 
 var jsonObject = JSON.parse(localStorage.getItem("sentArticle"));
 
-var text = '<br><br><div class=""';
+var text = '<br><br><br>';
 if (jsonObject.author == null || jsonObject.author == '') text += '<p>Written by: Unknown</p>';
 else text += '<p>Written by: ' + jsonObject.author + '</p>';
 text += '<p>Published at: ' + (jsonObject.publishedAt.replace("T", " ")).replace("Z", " ") + '</p>';
 text += '<h1>' + jsonObject.title + '</h1>';
-text += '<div class=""><img class="" src=' + jsonObject.urlToImage + '></div>';
+text += '<img class="imgcenter" src=' + jsonObject.urlToImage + '>';
 text += '<h4>' + jsonObject.description + '</h4>';
-text += '<br>';
 text += '<p>For more information click <a href="' + jsonObject.url + '">here</a></p>';
-text += '</div><br><br>';
+text += '<br><br>';
 
 document.getElementById("Article").innerHTML = text;
--- nobootstrap/scripts/IndexScript.js
+++ nobootstrap/scripts/IndexScript.js
@@ -10,17 +10,17 @@ async function getNews() {
 
     function ReturnArticle(i) {
         jsonText.push(JSON.stringify(data.articles[i]));
-        return articleString = '<div class=""><div class=""><a class="ArticleLink" onclick="SendJson(' + i + ')" href="pages/DetailedView.html"><h2 class="overflow">' + data.articles[i].title + '</h2><img width="100%" height="400" src=' + data.articles[i].urlToImage + '></a></div></div>';
+        return articleString = '<a class="ArticleLink" onclick="SendJson(' + i + ')" href="pages/DetailedView.html"><h2 class="overflow">' + data.articles[i].title + '</h2><img width="100%" height="100%" src=' + data.articles[i].urlToImage + '></a>';
     }
 
     const response = await fetch(url);
     const data = await response.json();
 
-    var text = '<br><br><div class="">';
+    var text = '<br><br>';
     for (var i = 0; i < 8; i++) {
         text += ReturnArticle(i);
     }
-    text += '</div><br><br>';
+    text += '<br><br>';
 
     document.getElementById('Articles').innerHTML = text;
 }
